c4645f7ccefe5d23ceb1b42ddd49298d09c6124c
Tim Shenin <t.shenin@celonis.com>
2020-10-27 00:11:13 +0100

    Added model

--- src/models/models.module.ts
+++ src/models/models.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ModelsController } from './models.controller';
 import { ModelsService } from './models.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Model } from './model.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Model])],
   controllers: [ModelsController],
-  providers: [ModelsService]
+  providers: [ModelsService],
 })
-export class ModelsModule {}
+export class ModelsModule {
+}
