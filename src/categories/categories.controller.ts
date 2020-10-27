d62b49befaf8dba9f37e946ed17b946080cc26ab
Anton Cherepov <chekitdnb@gmail.com>
2020-10-27 23:35:34 +0100

    Added main methods, added utils folder, updated gitignore

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,10 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    ParseIntPipe,
+    Post,
+    Put,
+} from '@nestjs/common';
 
 @Controller('categories')
 export class CategoriesController {
-
     @Get()
-    getCategories(): any {
-        return 'categories list';
+    getCategoryList(): any {
+        return '[GET] category list';
+    }
+
+    @Post()
+    createCategory(@Body() categoryDto: any): any {
+        return `[POST] Create category: ${JSON.stringify(categoryDto)}`;
+    }
+
+    @Get(':id')
+    getCategoryById(@Param('id', ParseIntPipe) id: number): any {
+        return `[GET] Category by id: ${id}`;
+    }
+
+    @Put(':id')
+    updateCategory(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() categoryDto: any,
+    ): any {
+        return `[PUT] Update category by id: ${id}, ${JSON.stringify(
+            categoryDto,
+        )}`;
+    }
+
+    @Delete(':id')
+    deleteCategory(@Param('id', ParseIntPipe) id: number): any {
+        return `[DELETE] Delete category by id : ${id}`;
     }
 }
