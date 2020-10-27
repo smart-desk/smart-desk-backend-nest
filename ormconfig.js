14d175b71ddc4ae95704d705458877a9d6fefff7
Anton Cherepov <chekitdnb@gmail.com>
2020-10-27 08:36:43 +0100

    fixed ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: ['dist/**/*.entity.js'],
+  entities: ['dist/**/*.entity.{ts, js}'],
   synchronize: false,
   migrationsTableName: 'nest_migrations',
-  migrations: ['src/migrations/**/*.ts'],
+  migrations: [__dirname + '/migrations/**/*.{ts, js}'],
   migrationsRun: true,
   cli: {
     migrationsDir: 'src/migrations'
