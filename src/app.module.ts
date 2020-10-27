4f5a2128068c3c2494f9043bb0bc62ea5e7766ab
Anton Cherepov <chekitdnb@gmail.com>
2020-10-27 22:57:57 +0100

    fix precommit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth';
 import { CategoriesModule } from './categories';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './users';
+
+const app = [AuthModule, UsersModule, CategoriesModule];
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), AuthModule, UsersModule, CategoriesModule, ConfigModule.forRoot()],
-  controllers: [],
-  providers: [],
-  exports: []
+    imports: [TypeOrmModule.forRoot(), ConfigModule.forRoot(), ...app],
+    controllers: [],
+    providers: [],
+    exports: [],
 })
-export class AppModule {
-}
+export class AppModule {}
