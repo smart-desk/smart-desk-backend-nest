1e71a82b97a9488f7b7ce3a1198dbe8006586683
Anton Cherepov <chekitdnb@gmail.com>
2020-11-12 00:18:45 +0100

    Add basic e2e

--- src/categories/categories.e2e-spec.ts
+++ src/categories/categories.e2e-spec.ts
@@ -1,6 +1,17 @@
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import * as request from 'supertest';
 import { CategoriesModule } from './categories.module';
+import { Category } from './entities/category.entity';
+
+const CategoriesServiceMock = {
+    create: jest.fn(),
+    delete: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+};
 
 describe('[Feature] Categories - /categories', () => {
     let app: INestApplication;
@@ -8,13 +19,19 @@ describe('[Feature] Categories - /categories', () => {
     beforeAll(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [CategoriesModule],
-        }).compile();
+        })
+            .overrideProvider(getRepositoryToken(Category))
+            .useValue(CategoriesServiceMock)
+            .compile();
 
         app = moduleFixture.createNestApplication();
         await app.init();
     });
 
-    it.todo('Get all [GET /]');
+    it('Get all [GET /]', () => {
+        request(app.getHttpServer()).get('/api/categories').send().expect(HttpStatus.OK);
+    });
+
     it.todo('Create [POST /]');
     it.todo('Get one [GET /:id]');
     it.todo('Update one [PATCH /:id]');
