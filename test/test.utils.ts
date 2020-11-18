b2dda058d04ec209f2ac15f02edfee966a0b9277
Anton Cherepov <chekitdnb@gmail.com>
2020-11-19 00:39:09 +0100

    Fix

--- test/test.utils.ts
+++ test/test.utils.ts
@@ -3,7 +3,11 @@ import { TestingModule } from '@nestjs/testing';
 
 export async function createTestAppForModule(moduleRef: TestingModule) {
     const app = moduleRef.createNestApplication();
-    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalPipes(
+        new ValidationPipe({
+            forbidNonWhitelisted: true,
+        })
+    );
     await app.init();
     return app;
 }
