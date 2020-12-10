d7bafc91384922a5e314aa88115bd07954539aec
Tim Shenin <t.shenin@celonis.com>
2020-12-10 22:02:53 +0100

    Fix type transformation for price

--- src/dynamic-fields/price/dto/create-price.dto.ts
+++ src/dynamic-fields/price/dto/create-price.dto.ts
@@ -1,8 +1,10 @@
 import { IsNumber, IsPositive } from 'class-validator';
 import { DynamicFieldsBaseCreateDto } from '../../dynamic-fields-base-create.dto';
+import { Type } from 'class-transformer';
 
 export class CreatePriceDto extends DynamicFieldsBaseCreateDto {
     @IsNumber()
     @IsPositive()
+    @Type(() => Number)
     value: number;
 }
