d76201592227903806e849014618b6a1ea519060
Tim Shenin <t.shenin@celonis.com>
2020-12-24 02:14:25 +0100

    Added filterable prop to radio

--- src/dynamic-fields/radio/dto/radio-params.dto.ts
+++ src/dynamic-fields/radio/dto/radio-params.dto.ts
@@ -1,10 +1,14 @@
-import { IsNotEmpty, IsString, MaxLength, ValidateNested } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, MaxLength, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class RadioParamsDto {
     @Type(() => RadioItem)
     @ValidateNested()
     radios: RadioItem[];
+
+    @IsOptional()
+    @IsBoolean()
+    filterable?: boolean = false;
 }
 
 export class RadioItem {
