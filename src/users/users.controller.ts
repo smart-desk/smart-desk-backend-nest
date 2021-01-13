ac7250380b90e59bc1e428e9ac6f3a4ef4453cff
Tim Shenin <t.shenin@celonis.com>
2021-01-13 21:23:22 +0100

    Get all users endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,12 @@
-import { Body, Controller, Get, Param, ParseUUIDPipe, Patch, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, ForbiddenException, Get, Param, ParseUUIDPipe, Patch, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { ACGuard, UseRoles } from 'nest-access-control';
-import { ResourceEnum } from '../app/app.roles';
-import { JWTUserPayload } from '../auth/jwt.strategy';
+import { ResourceEnum, RolesEnum } from '../app/app.roles';
+import { JWTPayload, JWTUserPayload } from '../auth/jwt.strategy';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
@@ -32,8 +32,25 @@ export class UsersController {
         return await this.usersService.updateUser(req.user.id, data);
     }
 
+    @Get()
+    @ApiBearerAuth('access-token')
+    @UseGuards(JwtAuthGuard, ACGuard)
+    @UseRoles({
+        resource: ResourceEnum.USER,
+        action: 'read',
+    })
+    async getUsers(@Req() req: Request & JWTUserPayload): Promise<User[]> {
+        const isAdmin = this.isAdmin(req.user);
+        if (!isAdmin) throw new ForbiddenException();
+        return await this.usersService.fineAll();
+    }
+
     @Get(':id')
     async getUser(@Param('id', ParseUUIDPipe) id: string): Promise<User> {
         return await this.usersService.fineOne(id);
+    }
+
+    private isAdmin(userPayload: JWTPayload): boolean {
+        return userPayload.roles && userPayload.roles.some(role => role === RolesEnum.ADMIN);
     }
 }
