ff8625ba442438e22b78810a8401b5bcf10bde07
Tim Shenin <t.shenin@celonis.com>
2021-07-12 22:48:57 +0200

    Convert to numbers

--- src/modules/ad/enitities/ad-config.entity.ts
+++ src/modules/ad/enitities/ad-config.entity.ts
@@ -1,4 +1,5 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Type } from 'class-transformer';
 
 @Entity('ad_config')
 export class AdConfig {
@@ -6,11 +7,14 @@ export class AdConfig {
     id: string;
 
     @Column('numeric', { name: 'main_hourly_rate', precision: 13, scale: 2 })
+    @Type(() => Number)
     mainHourlyRate: number;
 
     @Column('numeric', { name: 'sidebar_hourly_rate', precision: 13, scale: 2 })
+    @Type(() => Number)
     sidebarHourlyRate: number;
 
     @Column('numeric', { name: 'lift_rate', precision: 13, scale: 2 })
+    @Type(() => Number)
     liftRate: number;
 }
