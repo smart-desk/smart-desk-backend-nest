28865714b9b75f01a24ef5003defa70935dcc2ad
Tim Shenin <t.shenin@celonis.com>
2021-07-22 23:09:36 +0200

    Fixed tests

--- test/test.utils.ts
+++ test/test.utils.ts
@@ -21,6 +21,7 @@ import { StripeService } from '../src/modules/stripe/stripe.service';
 import { AdConfig } from '../src/modules/ad/enitities/ad-config.entity';
 import { AdCampaign } from '../src/modules/ad/enitities/ad-campaign.entity';
 import { PromoSet } from '../src/modules/promo/entities/promo-set.entity';
+import { Promo } from '../src/modules/promo/entities/promo.entity';
 
 export async function createTestAppForModule(moduleRef: TestingModule) {
     const app = moduleRef.createNestApplication();
@@ -99,6 +100,8 @@ export function declareCommonProviders(moduleRef: TestingModuleBuilder): Testing
         .overrideProvider(getRepositoryToken(AdCampaign))
         .useValue(createRepositoryMock())
         .overrideProvider(getRepositoryToken(PromoSet))
+        .useValue(createRepositoryMock())
+        .overrideProvider(getRepositoryToken(Promo))
         .useValue(createRepositoryMock())
         .overrideProvider(MailService)
         .useValue(MailServiceMock)
