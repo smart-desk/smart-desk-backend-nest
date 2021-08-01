4bf8f1a6f0c11dafe85b93b758f5cd2776b8e6e0
Tim Shenin <t.shenin@celonis.com>
2021-08-01 20:39:20 +0200

    Use relative path for images

--- src/modules/dynamic-fields/photo/dto/create-photo.dto.ts
+++ src/modules/dynamic-fields/photo/dto/create-photo.dto.ts
@@ -1,13 +1,10 @@
-import { IsArray, IsNotEmpty, IsString, IsUrl, Max, MaxLength } from 'class-validator';
+import { IsArray, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 import { DynamicFieldsBaseCreateDto } from '../../dynamic-fields-base-create.dto';
-import { IsImageUrl } from '../../../../utils/validation';
 
 export class CreatePhotoDto extends DynamicFieldsBaseCreateDto {
     @IsNotEmpty()
     @IsArray()
     @MaxLength(1000, { each: true })
     @IsString({ each: true })
-    @IsUrl({}, { each: true })
-    @IsImageUrl({ each: true })
     value: string[];
 }
