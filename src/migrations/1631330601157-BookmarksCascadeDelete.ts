5e39aa4f8a2b64e4d0b3fbc7a2d95b6370564d3d
Tim Shenin <t.shenin@celonis.com>
2021-09-11 18:17:16 +0700

    Prod foreign key

--- src/migrations/1631330601157-BookmarksCascadeDelete.ts
+++ src/migrations/1631330601157-BookmarksCascadeDelete.ts
@@ -2,7 +2,7 @@ import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
 
 export class BookmarksCascadeDelete1631330601157 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropForeignKey('bookmarks', 'FK_e1567c13b31c71230d7310db69c');
+        await queryRunner.dropForeignKey('bookmarks', 'FK_05c5b94a5fbfaec47b1632bf5b1');
 
         await queryRunner.createForeignKey(
             'bookmarks',
