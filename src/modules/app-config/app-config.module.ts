074cc283fa5a441169faa286b75c313034b8a103
Tim Shenin <t.shenin@celonis.com>
2021-11-14 23:08:44 +0100

    Move image to public folder

--- src/modules/app-config/app-config.module.ts
+++ src/modules/app-config/app-config.module.ts
@@ -4,9 +4,10 @@ import { UsersModule } from '../users/users.module';
 import { AppConfig } from './enitities/app-config.entity';
 import { AppConfigController } from './app-config.controller';
 import { AppConfigService } from './app-config.service';
+import { S3Module } from '../s3/s3.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([AppConfig]), forwardRef(() => UsersModule)],
+    imports: [TypeOrmModule.forFeature([AppConfig]), forwardRef(() => UsersModule), forwardRef(() => S3Module)],
     controllers: [AppConfigController],
     providers: [AppConfigService],
     exports: [AppConfigService],
--- src/modules/app-config/app-config.service.ts
+++ src/modules/app-config/app-config.service.ts
@@ -3,14 +3,18 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AppConfig } from './enitities/app-config.entity';
 import { AppConfigDto } from './dto/app-config.dto';
+import { S3Service } from '../s3/s3.service';
 
 @Injectable()
 export class AppConfigService {
-    constructor(@InjectRepository(AppConfig) private appConfigRepository: Repository<AppConfig>) {}
+    constructor(@InjectRepository(AppConfig) private appConfigRepository: Repository<AppConfig>, private s3Service: S3Service) {}
 
     async updateAppConfig(newConfig: AppConfigDto): Promise<AppConfig> {
+        if (newConfig?.logo) {
+            await this.s3Service.moveImageToPublic(newConfig?.logo);
+            newConfig.logo = newConfig?.logo.replace('temp', 'public');
+        }
         const oldConfig = await this.appConfigRepository.findOne();
-
         if (oldConfig) {
             const updatedConfig = await this.appConfigRepository.preload({ id: oldConfig.id, ...newConfig });
             return await this.appConfigRepository.save(updatedConfig);
